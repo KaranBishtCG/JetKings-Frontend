8fe4fb1b51218e4dc0475484d10b16fedbea3bff
Karan Singh Bisht <karan.bisht@cginfinity.com>
2026-07-10 13:34:10 +0530

    Folder Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import {Routes, Route} from "react-router-dom";
+import Layout from './layout/Layout';
+import Home from './pages/Home';
+import About from './pages/About';
 
 function App() {
   return (
-    <div>App</div>
+    <Routes>
+      <Route path="/" element={<Layout />}> 
+        <Route index element={<Home />} /> 
+      </Route>
+    </Routes>
   )
 }
 
