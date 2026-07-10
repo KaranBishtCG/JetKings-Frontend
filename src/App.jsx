a20f98a95bd09c3ea26614f19f871aa5dc695092
sangya25-cg <sangya.ojha@cginfinity.com>
2026-07-10 15:54:07 +0530

    login + billing

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,20 @@ import Buyers from './pages/Buyers';
 import Categories from './pages/Categories';
 import PriceConfig from './pages/PriceConfig';
 import Products from './pages/Products';
+import Login from './pages/Login';
+import Invoice from "./pages/Invoice";
 
 function App() {
   return (
     <Routes>
+      <Route path="/login" element={<Login />} />
       <Route path="/" element={<Layout />}> 
         <Route index element={<Home />} /> 
         <Route path="/buyers" element={<Buyers/>} />
         <Route path="/categories" element={<Categories/>} />
         <Route path="/products" element={<Products/>} />
         <Route path="/price-config" element={<PriceConfig/>} />
+        <Route path="/invoice" element={<Invoice />} />
       </Route>
     </Routes>
   )
